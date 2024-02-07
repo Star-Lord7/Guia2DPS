ebb2b043a8408e37eedd38f2a7f0eae6d7d0b9f2
Star-Lord7 <131508387+Star-Lord7@users.noreply.github.com>
2024-02-06 20:48:52 -0600

    Ejemplo3

--- src/app/page.js
+++ src/app/page.js
@@ -1,58 +1,82 @@
+"use client";
+import { useState } from "react";
 import styles from "./page.module.css";
-const Equipos = ({ equipos }) => {
-  return (
-    <div className={styles.container__list}>
-    <h2 className={styles.title}>Equipos de Fútbol</h2>
-
-      {equipos.map((equipo) => (
-        <div key={equipo.id}>
-          <h3 className={styles.nameclub}>{equipo.nombre}</h3>
-          <ul >
-
-            {equipo.plantilla.map((jugador) => (
-              <li className={styles.container__list} key={jugador.id}> <img src={jugador.imagen} className={styles.playerImage}/>
-              <strong>{jugador.nombre}</strong>
-              <p>Altura: {jugador.Altura}m <br></br> Peso:{jugador.Peso}Kg</p>
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 export default function Home() {
-  // Simula la obtención de datos desde tu JSON
-const equiposData = [
-  {"id": 1,
-  "nombre": "Real Madrid",
-  "plantilla":[
-  {"id": 1,"imagen":"https://phantom-marca-mx.unidadeditorial.es/df664815ec965603571f9a5cf0a1f195/resize/828/f/jpg/mx/assets/multimedia/imagenes/2024/02/03/17069586953729.jpg", "nombre":"Eden Hazard","Altura":"1.75","Peso":"74Kg"},
-  {"id": 2,"imagen":"https://s.hs-data.com/bilder/spieler/gross/600867.jpg","nombre":"Gonzalo García","Altura":"1.82","Peso":"74Kg"},
-  {"id": 3,"imagen":"https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Karim_Benzema_wearing_Real_Madrid_home_kit_2021-2022.jpg/1200px-Karim_Benzema_wearing_Real_Madrid_home_kit_2021-2022.jpg", "nombre":"Karim Benzema","Altura":"1.85","Peso":"81Kg"}
-  ]},
-  {"id": 2,
-  "nombre": "Barcelona",
-  "plantilla":[
-  {"id": 1, "imagen":"https://s.hs-data.com/bilder/spieler/gross/150720.jpg","nombre":"Marc-André ter Stegen","Altura":"1.75","Peso":"74Kg"},
-  {"id": 2,"imagen":"https://cdn.vox-cdn.com/thumbor/kyjvmBK9Mk_12BdVpIo2f4IFYX8=/0x0:2357x1571/1200x800/filters:focal(984x110:1360x486)/cdn.vox-cdn.com/uploads/chorus_image/image/73032733/1793442924.0.jpg","nombre":"Iñigo Martinez","Altura":"1.82","Peso":"74Kg"},
-  {"id": 3,"imagen":"https://s.hs-data.com/bilder/spieler/gross/511270.jpg","nombre":"Gavi","Altura":"1.85","Peso":"81Kg"}
-  ]},
-  {"id": 3,
-  "nombre": "Atletico de Madrid",
-  "plantilla":[
-  {"id": 1,"imagen":"https://upload.wikimedia.org/wikipedia/commons/thumb/a/a1/Jan_Oblak_2019.jpg/800px-Jan_Oblak_2019.jpg","nombre":"Jan Oblack","Altura":"1.75","Peso":"74Kg"},
-  {"id": 2,"imagen":"https://s.hs-data.com/bilder/spieler/gross/153546.jpg","nombre":"Antoine Griezmann","Altura":"1.82","Peso":"74Kg"},
-  {"id": 3,"imagen":"https://www.diez.hn/binrepository/fmx77xxwiaenw8_3491166_20230118141235.jpg","nombre":"Memphis Depay","Altura":"1.85","Peso":"81Kg"}
-  ]}
-  ];
-  return (
-  <main className={styles.main}>
-  <div>
-  <h1>Mi Aplicación de Fútbol</h1>
-  <Equipos equipos={equiposData} />
-  </div>
-  </main>
-  );
+const [numero1, setNumero1] = useState('');
+const [numero2, setNumero2] = useState('');
+const [resultado, setResultado] = useState(null);
+
+const sumar = () => {
+const resultadoSuma = parseFloat(numero1) + parseFloat(numero2);
+setResultado(`Resultado de la suma: ${resultadoSuma}`);
+};
+
+const restar = () => {
+const resultadoResta = parseFloat(numero1) - parseFloat(numero2);
+setResultado(`Resultado de la resta: ${resultadoResta}`);
+};
+
+const multiplicar = () => {
+  const resultadoMultiplicar = parseFloat(numero1) * parseFloat(numero2);
+  setResultado(`Resultado de la multiplicacion: ${resultadoMultiplicar}`);
+};
+
+const dividir = () => {
+  if (parseFloat(numero2) === 0) {
+    setResultado("No se puede dividir entre cero");
+  } else {
+    const resultadoDividir = parseFloat(numero1) / parseFloat(numero2);
+    setResultado(`Resultado de la división: ${resultadoDividir}`);
   }
+};
+
+const Raiz = () => {
+  const resultadoRaiz = parseFloat(numero1);
+  if (numero1 < 0) {
+    setResultado("No se puede calcular la raíz cuadrada de un número negativo");
+  } else {
+    const resultadoRaiz = Math.sqrt(numero1);
+    setResultado(`Raíz cuadrada de ${numero1} es: ${resultadoRaiz}`);
+  }
+};
+
+
+return (
+<main className={styles.main}>
+<div className={styles.calculadora}>
+<div className={styles.numeros}>
+
+<label className={styles.text}>Número 1:</label>
+<input className={styles.inputnum} type="number"
+value={numero1} onChange={(e) => setNumero1(e.target.value)} />
+</div>
+
+<div className={styles.numeros}>
+<label className={styles.text} >Número 2:</label>
+<input className={styles.inputnum} type="number"
+value={numero2} onChange={(e) => setNumero2(e.target.value)} />
+</div>
+
+<div>
+<button className={styles.button}
+onClick={sumar}>Sumar</button>
+
+<button className={styles.button}
+onClick={restar}>Restar</button>
+
+<button className={styles.button}
+onClick={multiplicar}>Multiplicar</button>
+
+<button className={styles.button}
+onClick={dividir}>Dividir</button>
+
+<button className={styles.button}
+onClick={Raiz}>Raiz Cuadrada</button>
+</div>
+
+{resultado && <div className={styles.resultado}>{resultado}</div>}
+</div>
+</main>
+);
+}
